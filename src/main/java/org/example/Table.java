757b194ae4fa4b3b997ac986541341f9244d25f4
saivelamuri <saivelamuri26@gmail.com>
2023-09-29 15:33:42 +0200

    added_Table_in_price_branch

---
+++ src/main/java/org/example/Table.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Table {
+
+    int tableNumber;
+    String isAvailable;
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public String getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(String isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+}
