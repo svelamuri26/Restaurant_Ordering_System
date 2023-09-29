d71f01280fa1c57c0b91db963fe6e8208d40b8d6
saivelamuri <saivelamuri26@gmail.com>
2023-09-29 16:52:59 +0200

    added restaurantupdate

--- src/main/java/org/example/Restaurant.java
+++ src/main/java/org/example/Restaurant.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class Restaurant {
+
+   private int tables ;
+
+    public int getTables() {
+        return tables;
+    }
+
+    public void setTables(int tables) {
+        this.tables = tables;
+    }
 }
