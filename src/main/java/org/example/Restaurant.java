f4ef5e28927c1450c2698298cef4ca00f823e605
nadine12344 <65829610+nadine12344@users.noreply.github.com>
2023-09-29 17:49:51 +0300

    added fooditemlist

--- src/main/java/org/example/Restaurant.java
+++ src/main/java/org/example/Restaurant.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Restaurant {
+    private FoodItem[] foodItemsList;
+
+    public FoodItem[] getFoodItemsList(){
+        return foodItemsList;
+    }
+
+    public void setFoodItemsList(FoodItem[] foodItemsList){
+        this.foodItemsList=foodItemsList;
+    }
 }
